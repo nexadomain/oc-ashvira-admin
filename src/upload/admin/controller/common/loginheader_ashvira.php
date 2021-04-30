d4a64f6d7d36c35662c47310373fff1538d9532d
nexadomain <77724079+nexadomain@users.noreply.github.com>
2021-05-01 04:40:40 +0530

    Add files via upload
    Initiate OC Ashvira Admin Project.
    Login & Forgotten Page of Admistrative Section is completed

---
+++ src/upload/admin/controller/common/loginheader_ashvira.php
@@ -0,0 +1,30 @@
+<?php
+class ControllerCommonLoginheaderAshvira extends Controller {
+	public function index() {
+		$data['title'] = $this->document->getTitle();
+
+		if ($this->request->server['HTTPS']) {
+			$data['base'] = HTTPS_SERVER;
+		} else {
+			$data['base'] = HTTP_SERVER;
+		}
+
+		$data['description'] = $this->document->getDescription();
+		$data['keywords'] = $this->document->getKeywords();
+		$data['links'] = $this->document->getLinks();
+		$data['styles'] = $this->document->getStyles();
+		$data['scripts'] = $this->document->getScripts();
+		$data['lang'] = $this->language->get('code');
+		$data['direction'] = $this->language->get('direction');
+
+		$this->load->language('common/header');
+
+		if (!isset($this->request->get['user_token']) || !isset($this->session->data['user_token']) || ($this->request->get['user_token'] != $this->session->data['user_token'])) {
+			$data['logged'] = '';
+
+			$data['home'] = $this->url->link('common/login', '', true);
+		}
+
+		return $this->load->view('common/loginheader_ashvira', $data);
+	}
+}
